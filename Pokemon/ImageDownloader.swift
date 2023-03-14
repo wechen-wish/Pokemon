b0eabf8b490bdc06abb3eb6ecc09e0d680b27f66
wechen-wish <66058016+wechen-wish@users.noreply.github.com>
2023-03-13 22:02:30 -0400

    Create README.md

--- Pokemon/ImageDownloader.swift
+++ Pokemon/ImageDownloader.swift
@@ -7,15 +7,13 @@
 
 import UIKit
 
+// official artwork: https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/{id}.png
+
 protocol ImageDownloadable {
     func loadImage(url: String, completion: ((UIImage?) -> Void)?)
-    func cancel()
 }
 
 final class ImageDownloader: ImageDownloadable {
     func loadImage(url: String, completion: ((UIImage?) -> Void)?) {
-    }
-    
-    func cancel() {
     }
 }
--- Pokemon/PokemonListService.swift
+++ Pokemon/PokemonListService.swift
@@ -7,26 +7,9 @@
 
 import Foundation
 
-// official artwork: https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/{id}.png
-
-
 
 struct PokemonListService {
     func request(offset: Int, completion: (([Pokemon]) -> Void)? = nil, failure: ((String?) -> Void)? = nil) {
-        guard let url = URL(string: "https://pokeapi.co/api/v2/pokemon?offset=\(offset)&limit=20") else { return }
-        DispatchQueue.global().async {
-            API().fetchData(url: url) { result in
-                switch result {
-                case let .success(data):
-                    guard  let rest = try? JSONDecoder().decode(Pokemons.self, from: data) else {
-                        completion?([])
-                        return
-                    }
-                    completion?(rest.results)
-                case .failure(_):
-                    completion?([])
-                }
-            }
-        }
+        
     }
 }
